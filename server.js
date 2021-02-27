776e9d4045a6d168ad0a96bc570e9e69bc7c8a7f
Shelsy Elaine <elainesbarrera@gmail.com>
2021-02-27 11:30:08 -0800

    FIXED: build command

--- server.js
+++ server.js
@@ -5,10 +5,10 @@ const PORT = process.env.PORT || 5000;
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
+app.use("/api", require("./routes/apiRoutes"));
+
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
 }
-
-app.use("/api", require("./routes/apiRoutes"));
 
 app.listen(PORT, () => console.log(`http://localhost:${PORT}`));
